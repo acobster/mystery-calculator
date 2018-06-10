9c07efde72b1b3a46a34d4fe5dbaec55f8004a50
Coby Tamayo <acobster@gmail.com>
2018-06-10 16:17:53 -0700

    resolve conflict w/ local changes

---
+++ src/MysteryCalculator.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import './index.css';
+
+import Page from './Page.js';
+import Answer from './Answer.js';
+import ModeButton from './ModeButton.js';
+
+class MysteryCalculator extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.PAGE_COUNT = 6;
+
+    this.state = {
+      mode: 'binary',
+      selectedPages: [],
+      reveal: false,
+    };
+
+    this.modes = [
+      { name: 'mystery',    title: 'Mystery Mode',    value: '?' },
+      { name: 'decimal',    title: 'Decimal Mode',    value: '32' },
+      { name: 'binary',     title: 'Binary Mode',     value: '0110' },
+      { name: 'explainer',  title: 'Explainer Mode',  value: 'Explain!' },
+    ];
+  }
+
+  handleModeChange(mode) {
+    this.setState({
+      mode: mode,
+      reveal: (mode !== 'mystery'),
+    });
+  }
+
+  togglePageSelected(i) {
+    let pages = this.state.selectedPages.slice();
+
+    if (this.pageSelected(i)) {
+      pages.splice(pages.indexOf(i), 1);
+    } else {
+      pages.push(i);
+    }
+
+    this.setState({
+      selectedPages: pages,
+      reveal: false
+    });
+  }
+
+  pageSelected(i) {
+    return this.state.selectedPages.indexOf(i) > -1;
+  }
+
+  calculate(pages) {
+    return pages.reduce((sum, n) => (sum + Math.pow(2, n)), 0);
+  }
+
+  render() {
+    let pages = Array(this.PAGE_COUNT).fill(null).map((_, i) => (
+      <Page
+        key={i}
+        number={i}
+        mode={this.state.mode}
+        onClick={() => this.togglePageSelected(i)}
+        selected={this.pageSelected(i)}
+      />
+    ));
+
+    let modeOptions = this.modes.map((mode, i) => {
+      return (
+        <li className="mode-option" key={i}>
+          <ModeButton
+            title={mode.title}
+            currentMode={this.state.mode}
+            mode={mode.name}
+            onClick={(mode) => this.handleModeChange(mode)}
+            value={mode.value}
+          />
+        </li>
+      );
+    });
+
+    return (
+      <div className="mystery-calculator">
+        <article className="pages">
+          {pages}
+        </article>
+        <ul className="modes">
+          {modeOptions}
+        </ul>
+        <aside className="answer">
+          <Answer
+            number={this.calculate(this.state.selectedPages)}
+            mode={this.state.mode}
+            reveal={this.state.reveal}
+            onReveal={() => this.setState({reveal: true})}
+          />
+        </aside>
+      </div>
+    );
+  }
+}
+
+export default MysteryCalculator;
