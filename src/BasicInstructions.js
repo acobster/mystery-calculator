c5d8bce56006668cbb06123bde53a3947e7e6598
Coby Tamayo <acobster@gmail.com>
2019-12-15 00:36:38 -0800

    refactor Explainer component

---
+++ src/BasicInstructions.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default () => (
+    <div>
+        <p>Choose a number from 1 to 63.</p>
+        <p>Click on each card where your number appears.</p>
+        <p>
+            Click "reveal," and the Mystery Calculator
+            will guess your number!
+        </p>
+        <p>Proceed to Decimal Mode to learn how this works!</p>
+    </div>
+)
--- src/Explainer.js
+++ src/Explainer.js
@@ -1,60 +1,25 @@
 import React from 'react'
 
+import BasicInstructions from './BasicInstructions'
 import DecimalExplainer from './DecimalExplainer'
 import BinaryExplainer from './BinaryExplainer'
 
-class Explainer extends React.Component {
-  renderBasicInstructions() {
-    return (
-      <div>
-        <p>Choose a number from 1 to 63.</p>
-        <p>Click on each card where your number appears.</p>
-        <p>
-          Click "reveal," and the Mystery Calculator
-          will guess your number!
-        </p>
-        <p>Proceed to Decimal Mode to learn how this works!</p>
-      </div>
-    )
+export default ({ selectedPages, number, mode }) => {
+  const explainersByMode = {
+    mystery:   <BasicInstructions />,
+    decimal:   <DecimalExplainer
+                  selectedPages={selectedPages}
+                  number={number}
+                />,
+    binary:    <BinaryExplainer
+                  selectedPages={selectedPages}
+                  number={number}
+                  mode={mode}
+                />,
+    explainer: <div>hey</div>
   }
 
-  renderDecimalExplainer() {
-    return <DecimalExplainer
-      selectedPages={this.props.selectedPages}
-      number={this.props.number}
-    />
-  }
-
-  renderBinary() {
-    return (
-      <BinaryExplainer
-        selectedPages={this.props.selectedPages}
-        number={this.props.number}
-        mode={this.props.mode}
-      />
-    )
-  }
-
-  render() {
-    let explanation
-
-    switch (this.props.mode) {
-      case 'binary':
-        explanation = this.renderBinary()
-        break;
-      case 'decimal':
-        explanation = this.renderDecimalExplainer()
-        break;
-      case 'mystery':
-      default:
-        explanation = this.renderBasicInstructions()
-        break;
-    }
-
-    return (
-      <aside className="explainer">{explanation}</aside>
-    )
-  }
+  return (
+    <aside className="explainer">{explainersByMode[mode]}</aside>
+  )
 }
-
-export default Explainer
