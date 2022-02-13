9ec2d39771e64a4cc2cfd9a8935fcb07bafc539a
Coby Tamayo <acobster@gmail.com>
2022-02-12 21:20:46 -0800

    refactor BinaryCalculation as a functional component

--- src/BinaryCalculation.js
+++ src/BinaryCalculation.js
@@ -1,27 +1,25 @@
 import React from 'react'
 import Num from './Num'
 
-class BinaryCalculation extends React.Component {
-  render() {
-    const addends = this.props.selectedPages.map((p) => {
-      return (
-        <div className="binary-addend" key={p}>
-          <Num number={Math.pow(2, p)} mode="binary" />
-        </div>
-      )
-    }, '')
-
+const BinaryCalculation = ({ number, selectedPages }) => {
+  const addends = selectedPages.map((p) => {
     return (
-      <div className="binary-calculation equation">
-        <div className="binary-addends">
-          {addends}
-        </div>
-        <div className="binary-sum">
-          <Num number={this.props.number} mode="binary" />
-        </div>
+      <div className="binary-addend" key={p}>
+        <Num number={Math.pow(2, p)} mode="binary" />
       </div>
     )
-  }
+  }, '')
+
+  return (
+    <div className="binary-calculation equation">
+      <div className="binary-addends">
+        {addends}
+      </div>
+      <div className="binary-sum">
+        <Num number={number} mode="binary" />
+      </div>
+    </div>
+  )
 }
 
 export default BinaryCalculation
